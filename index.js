f5b79daecee470fe4ac810f4343200ca10dfa112
Joir Neto <36131984+joirneto@users.noreply.github.com>
2022-02-13 13:10:00 -0300

    Config initial

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+const indexRouter = require('./routes')
+
+app.set('views',path.join(__dirname, 'views'));
+app.set('views engine', 'ejs');
+
+app.use('/', indexRouter)
+
+app.listen(PORT, ()=>{
+    console.log("Servidor ok")
+})
