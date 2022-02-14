7c461d3d64a9b45483871706374c2f2ac6c218b9
Joir Neto <36131984+joirneto@users.noreply.github.com>
2022-02-14 16:41:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const tabuadaController = require('./routes/tabuada')
 app.set('views',path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('public'));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter)
 app.use('/tabuadas', tabuadaController)
