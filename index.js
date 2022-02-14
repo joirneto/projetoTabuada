3f64edf14539751fa089b3e3035b0708fe7ee812
Joir Neto <36131984+joirneto@users.noreply.github.com>
2022-02-14 11:28:51 -0300

    tabuada

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-const indexRouter = require('./routes')
+const indexRouter = require('./routes/index')
+const tabuadaController = require('./routes/tabuada')
 
 app.set('views',path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -11,6 +12,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.use('/', indexRouter)
+app.use('/tabuadas', tabuadaController)
 
 app.listen(PORT, ()=>{
     console.log("Servidor ok")
